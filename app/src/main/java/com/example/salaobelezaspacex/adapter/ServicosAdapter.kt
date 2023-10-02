8d2f8563eb6bd8ece8f4455f18c8a0b87ea38830
CarolShiny <carolinecg431@gmail.com>
2023-10-02 02:31:59 -0300

    Implementos da home

---
+++ app/src/main/java/com/example/salaobelezaspacex/adapter/ServicosAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.salaobelezaspacex.adapter
+
+class ServicosAdapter {
+}
--- app/src/main/java/com/example/salaobelezaspacex/model/Servicos.kt
+++ app/src/main/java/com/example/salaobelezaspacex/model/Servicos.kt
@@ -1,5 +1,6 @@
 package com.example.salaobelezaspacex.model
 
-class Servicos {
-
-}
+data class Servicos (
+    val img: Int? = null,
+    val nome: String? = null
+        )
