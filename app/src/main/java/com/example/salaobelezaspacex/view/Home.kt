69177adf02ada6dc62e3d9ac327266c1dcd87ae0
ReinaldoFM-taleknight <reinaldo.1001@hotmail.com>
2023-10-07 05:30:17 -0300

    Opções de agendamentos, concluidas! adaptando código

--- app/src/main/java/com/example/salaobelezaspacex/view/Home.kt
+++ app/src/main/java/com/example/salaobelezaspacex/view/Home.kt
@@ -2,6 +2,10 @@ package com.example.salaobelezaspacex.view
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -16,7 +20,6 @@ import com.example.salaobelezaspacex.adapter.ServicosAdapter
 import com.example.salaobelezaspacex.databinding.ActivityHomeBinding
 import com.example.salaobelezaspacex.model.Servicos
 
-
 class Home : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -26,7 +29,6 @@ class Home : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -39,6 +41,7 @@ class Home : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_home)
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
@@ -48,6 +51,59 @@ class Home : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val btnAgendar = findViewById<Button>(R.id.btAgendar)
+        val radioGroup1 = findViewById<RadioGroup>(R.id.radioGroup1)
+        val radioGroup2 = findViewById<RadioGroup>(R.id.radioGroup2)
+
+        btnAgendar.setOnClickListener(View.OnClickListener {
+            // Lógica de seleção para radioGroup1
+            val selectedRadioButtonId1 = radioGroup1.checkedRadioButtonId
+            when (selectedRadioButtonId1) {
+                R.id.radioButton1 -> {
+                    // Faça algo com a seleção do radioButton1 de radioGroup1
+                    print("Radio Button 11111 selecionado")
+                    println()
+                    val radioButton1 = findViewById<RadioButton>(R.id.radioButton1)
+                    val textRadioButton1 = radioButton1.text
+                    println(textRadioButton1)
+
+                }
+                R.id.radioButton2 -> {
+                    // Faça algo com a seleção do radioButton2 de radioGroup1
+                    print("Radio Button 22222 selecionado")
+                    println()
+                    val radioButton2 = findViewById<RadioButton>(R.id.radioButton2)
+                    val textRadioButton2 = radioButton2.text
+                    println(textRadioButton2)
+
+                }
+                // Outros casos para radioGroup1
+            }
+
+            // Lógica de seleção para radioGroup2
+            val selectedRadioButtonId2 = radioGroup2.checkedRadioButtonId
+            when (selectedRadioButtonId2) {
+                R.id.radioButton3 -> {
+                    // Faça algo com a seleção do radioButton3 de radioGroup2
+                    print("Radio Button 33333 selecionado")
+                    println()
+                    val radioButton3 = findViewById<RadioButton>(R.id.radioButton3)
+                    val textRadioButton3 = radioButton3.text
+                    println(textRadioButton3)
+
+                }
+                R.id.radioButton4 -> {
+                    // Faça algo com a seleção do radioButton4 de radioGroup2
+                    print("Radio Button 4444444 selecionado")
+                    println()
+                    val radioButton4 = findViewById<RadioButton>(R.id.radioButton4)
+                    val textRadioButton4 = radioButton4.text
+                    println(textRadioButton4)
+                }
+                // Outros casos para radioGroup2
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
